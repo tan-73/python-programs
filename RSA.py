a7973ec497b153f90f4a163b799ddeb3ff6d0d13
Tanmay <anandtanmay474@gmail.com>
2024-04-24 22:57:20 +0530

    Updated function to generate prime number

--- RSA.py
+++ RSA.py
@@ -7,30 +7,16 @@ def isPrime(number) :
             return False
     return True
 
-p = random.randrange(214783, 2147483)
-q = random.randrange(214783, 2147483)
-print(p)
-print(q)
+def generate_prime() : 
+    prime = random.randint(7813, 78263)
+    while not isPrime(prime) : 
+        prime = random.randint(7813, 78263)
+    return prime
+
+
 
 str1 = "Hello World"
 # lst_str1 = [ch for ch in str1]
 # print(lst_str1)
 lst_ascii = [ord(ch) for ch in str1]
 print(lst_ascii)
-
-
-def gcd(num1, num2) :
-    num1 = float(num1)
-    num2 = float(num2) 
-    i = 1
-    while i < num1 or i < num2 : 
-        if num1 % i == 0 and num2 % i == 0 : 
-            gcd = i
-        i += 1
-    print(gcd)
-
-print(4%2)
-
-m = pow(37, 43) % 77
-print(m)
-gcd(p, q)
