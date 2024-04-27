d28c7b0eb50b580eae06ac93e252ea175a011790
Tanmay <anandtanmay474@gmail.com>
2024-04-27 22:45:26 +0530

    Changes

--- binary_encrypt.py
+++ binary_encrypt.py
@@ -3,9 +3,21 @@
 
 
 def toAscii(message) : 
+    binary_digits = []
     ascii_message = [ord(ch) for ch in message]
     print(ascii_message)
+    toBinary(ascii_message)
 
+def toBinary(ascii_message) :
+    binary_digits = []
+    binary = ""
+    for i in range(0, len(ascii_message)) : 
+        while ascii_message[i] > 0 :
+            remainder = ascii_message[i] % 2 
+            binary = str(remainder) + binary
+            ascii_message[i] //= 2
+            binary_digits.append(binary)
+    print(binary_digits)
 
 
 
