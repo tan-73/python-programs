b63fa73941fcb30c34ef38e3cb2d4fa9d9bfe3e5
Tanmay <anandtanmay474@gmail.com>
2024-04-28 22:36:47 +0530

    Defined function to convert to ascii and binary

--- binary_encrypt.py
+++ binary_encrypt.py
@@ -1,26 +1,30 @@
 '''input a string of characters, convert each character to it's corresponding ascii, then convert ascii to binary. Then we have a list of binary of the particular string'''
 
 
+def encode(message) : 
+    binary_message = []
+    ascii_message = toAscii(message)
+    for x in ascii_message : 
+        binary_message.append(toBinary(x))
+    print(ascii_message)
+    print(binary_message)
+
+
 
 def toAscii(message) : 
     binary_digits = []
     ascii_message = [ord(ch) for ch in message]
-    print(ascii_message)
-    toBinary(ascii_message)
+    return ascii_message
 
-def toBinary(ascii_message) :
-    binary_digits = []
+def toBinary(x) :
     binary = ""
-    for i in range(0, len(ascii_message)) : 
-        while ascii_message[i] > 0 :
-            remainder = ascii_message[i] % 2 
-            binary = str(remainder) + binary
-            ascii_message[i] //= 2
-            binary_digits.append(binary)
-    print(binary_digits)
+    while x > 0 : 
+        binary = str(x % 2) + binary
+        x //= 2
+    return binary
 
 
 
 #main
 message = "Hello World"
-toAscii(message)
+encode(message)
